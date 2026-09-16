d0386ba74db829b165974f3b8d8c6b8d2572d2a4
EA - Chrystopher Da Silva Lopes <lopes_chrystopher@acad.unasp.edu.br>
2026-09-15 22:43:14 -0300

    Esqueci de fazer os commits, paia

--- atividade_01.py
+++ atividade_01.py
@@ -1,1 +1,20 @@
-print("Hello Mundo!")
+## A atividade de se ver quantos números primos existem antes do número 67 (seis e sete)
+def pesquisa_binaria(lista, item):
+    baixo = 0
+    alto = len(lista) - 1
+
+    while baixo <= alto:
+        meio = (baixo + alto) // 2
+        chute = lista[meio]
+        if chute == item:
+            return meio
+        elif chute > item:
+            alto = meio - 1
+        else:
+            baixo = meio + 1
+    return None
+
+primos = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+resultado = pesquisa_binaria(primos, 67)
+print("Existem", resultado, "números primos de menor valor a 67.")
